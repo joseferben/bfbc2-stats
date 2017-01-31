cd2b15be05ecdd595a7c5b0b6093d74fb67e1c81
Josef Erben <josef@erben.io>
2017-01-31 14:27:39 +0100

    init

---
+++ entry.js
@@ -0,0 +1,4 @@
+import 'jquery';
+import 'bootstrap-loader';
+import './css/styles.scss';
+import './app/index.html';
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: ['./entry.js'],
+    output: {
+        path: path.join(__dirname, '/app'),
+        filename: 'bundle.js',
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './app',
+        proxy: {
+            '/api/*': {
+                target: 'http://localhost:8081/api',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': '',
+                },
+            },
+        },
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            riot: 'riot',
+        }),
+    ],
+
+    module: {
+        preLoaders: [{
+            test: /\.tag$/,
+            exclude: /node_modules/,
+            loader: 'riotjs-loader',
+            query: {
+                type: 'none',
+            },
+        }],
+        loaders: [{
+            test: /\.js$|\.tag$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader?presets[]=es2015&retainLines=true',
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader',
+        }, {
+            test: /\.scss$/,
+            loaders: ["style-loader", "css-loader", "sass-loader"]
+        }, {
+            test: /\.html$/,
+            loader: 'raw-loader',
+        }, {
+            test: /\.png$/,
+            loader: 'url-loader?limit=100000',
+        }, {
+            test: /\.jpg$/,
+            loader: 'file-loader',
+        }, {
+            test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=application/font-woff',
+        }, {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=application/octet-stream',
+        }, {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'file',
+        }, {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=image/svg+xml',
+        }, {
+            test: /jquery/,
+            loader: 'expose?$!expose?jQuery',
+        }, {
+            test: /\.json$/,
+            loader: 'json',
+        }],
+    },
+};
