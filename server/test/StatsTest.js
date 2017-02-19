51b7a3aa066dd78c7a93fd6ec4997cb0d8b927c6
Josef Erben <josef@erben.io>
2017-02-19 13:21:19 +0100

    Add smoke tests, split up Mocks.js into KillMocks.js and SessionsMocks.js because it is easier to navigate through them.

--- server/test/StatsTest.js
+++ server/test/StatsTest.js
@@ -1,14 +1,15 @@
 const expect = require('expect.js');
 
 const Stats = require('../Stats.js');
-const Mocks = require('./Mocks.js');
+const KillMocks = require('./mocks/KillMocks.js');
+
+const sut = Stats;
 
 describe('StatsTest ', () => {
 
     describe('getWeaponStats ', () => {
 
         it('should return empty array given empty array', () => {
-            const sut = Stats;
             const actual = sut.getWeaponStats([]);
 
             expect(actual).to.be.an('array');
@@ -16,13 +17,35 @@ describe('StatsTest ', () => {
         });
 
         it('should return stats for each weapon given an array of kills', () => {
-          const sut = Stats;
-          const actual = sut.getWeaponStats(Mocks.simpleKills());
-          const expected = Mocks.weaponStats();
+            const actual = sut.getWeaponStats(KillMocks.simpleKills());
+            const expected = KillMocks.weaponStats();
 
-          expect(actual).to.eql(expected);
+            expect(actual).to.eql(expected);
         });
 
     });
+
+    describe('_getConnections ', () => {
+
+        it('should return zero connections given an empty array of sessions', () => {
+            const actual = sut._getConnections([]);
+
+            expect(actual).to.be.an('number');
+            expect(actual).to.eql(0);
+        });
+
+    });
+
+    describe('_getTimePlayed ', () => {
+
+        it('should return zero seconds given an empty array of sessions', () => {
+            const actual = sut._getTimePlayed([]);
+
+            expect(actual).to.be.an('number');
+            expect(actual).to.eql(0);
+        });
+
+    });
+
 
 });
